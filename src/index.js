759c4c03e6816f5164fa1619f95e9a83863f6450
Jonathan Crider <>
2021-02-20 11:54:49 -0500

    more setup

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+
+const userList = document.querySelector('#users');
+const restaurantList = document.querySelector('#restaurants');
+const reservationsList = document.querySelector('#reservations');
+
+let users;
+
